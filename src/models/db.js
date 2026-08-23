2ce48808ae407e25e73e15adc6ea6029a0ae1b50
Milena Silva <mileninhaborghes@hotmail.com>
2026-08-23 19:12:32 -0300

    Atualizando conexão com o TiDB e SSL

--- src/models/db.js
+++ src/models/db.js
@@ -2,14 +2,19 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); 
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD, 
-    database: 'biblioteca_emocional_bd',
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
 });
-
 
 module.exports = pool;
